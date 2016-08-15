84e68fcfb87d1f49e102d63ed8f8ceef8430a94f
ComplYue <compl.yue@icloud.com>
2016-08-15 13:27:10 +0800

    Interoperability module for import. Provide null<=>None, true<=>True, false<=>False mappings atm.

---
+++ hbi/interop/__init__.py
@@ -0,0 +1,6 @@
+# this module meant to be imported by modules serving as HBI contexts, to provide adaptive behaviors across different
+# languages/runtimes of the hosting environment
+
+null = None
+true = True
+false = False
