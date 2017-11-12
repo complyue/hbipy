a10eb007b8997f03e59ef98c08bf1860a2d5f782
ComplYue <compl.yue@icloud.com>
2017-11-12 17:15:03 +0800

    update default log format

--- hbi/log.py
+++ hbi/log.py
@@ -11,9 +11,14 @@ hbi_root_logger = logging.getLogger('hbi')
 if len(hbi_root_logger.handlers) <= 0:
     # This means logging is not configured otherwise for HBI, so we can do our defaults
 
-    hbi_root_logger.handlers.append(
-        logging.StreamHandler(sys.stderr)
-    )
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(logging.Formatter(
+        "[%(asctime)s %(process)d](%(name)s) %(message)s\n"
+        ' -%(levelname)s- File "%(pathname)s", line %(lineno)d, in %(funcName)s',
+        "%FT%T"
+    ))
+
+    hbi_root_logger.handlers.append(handler)
     log_level = logging.INFO
     log_level_name = os.environ.get('HBI_LOG_LEVEL', 'INFO')
     try:
